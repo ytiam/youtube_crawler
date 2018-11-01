065b9edba7243489240b1a2114413b1358f47208
atanu1ytiam <amrkmrc@gmail.com>
2018-11-01 23:51:37 +0530

    Updated Scripts

--- crawler.py
+++ crawler.py
@@ -1,6 +1,6 @@
 from youtube import youtube
 
-search_term = 'coldplay yellow'
-output=youtube(search_term) #like youtube('pink floyd high hopes')
+search_term = 'coldplay yellow' #your desired search term
+output=youtube(search_term) 
 
 print(output)
